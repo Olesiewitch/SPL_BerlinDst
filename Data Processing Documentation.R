26ee5fea36f0d087802456c570d338a1811193bb
Olesiewitch <malgorzata.olesiewicz@gmail.com>
2018-12-08 16:00:55 +0100

    data processing

---
+++ Data Processing Documentation.R
@@ -0,0 +1,33 @@
+#### LIVIBILITY INDEX
+## DATA PROCESSING
+## Following data have been downloaded from https://de.statista.com on the 8 of December 2018.: 
+# "statistic_id12314_anzahl-der-sportvereine-in-berlin-bis-2018.xlsx"                                              
+# "statistic_id13353_hundehalter-und-hunde-im-bundesland-berlin-nach-bezirken-2016.xlsx"                           
+# "statistic_id154880_einwohnerzahl-in-berlin-bis-2017.xlsx"                                                       
+# "statistic_id252452_bruttoinlandsprodukt-je-erwerbstaetigen-in-berlin-bis-2017.xlsx"                             
+# "statistic_id252943_eigentumswohnungen-in-berlin---kaufpreise-2017-nach-bezirken.xlsx"                           
+# "statistic_id253201_arbeitnehmerentgelt-je-arbeitnehmer-in-berlin-bis-2017.xlsx"                                 
+# "statistic_id255791_bevoelkerungsdichte-in-berlin-bis-2014.xlsx"                                                 
+# "statistic_id255796_auslaenderanteil-in-berlin-bis-2017.xlsx"                                                    
+# "statistic_id259702_wohnflaeche-pro-einwohner-in-berlin-nach-bezirken-2016.xlsx"                                 
+# "statistic_id259905_mietpreise-in-berlin-2017-nach-bezirken.xlsx"                                                
+# "statistic_id36110_freie-ausbildungsplaetze-und-unvermittelte-bewerber-in-berlin-bis-2017.xlsx"                  
+# "statistic_id5010_bruttoinlandsprodukt-von-berlin-bis-2017.xlsx"                                                 
+# "statistic_id551758_verteilung-der-kaufpreisangebote-fuer-ein--zweifamilienhaeuser-in-berlins-bezirken-2017.xlsx"
+# "statistic_id652680_strassenbaeume-in-berlin-nach-bezirken-2017.xlsx"                                            
+# "statistic_id652716_oeffentliche-gruenflaechen-in-berlin-nach-bezirken-2017.xlsx"                                
+# "statistic_id653180_babys-pro-1000-einwohner-in-berlin-nach-altbezirken-2016.xlsx"                               
+#"statistic_id692779_wohngebaeude-in-berlin---bestand-nach-bezirken-2017.xlsx"                                    
+# "statistic_id762444_monatliche-arbeitslosenquote-in-berlin-bis-oktober-2018.xlsx"                                
+# "statistic_id864931_gewalttaten-auf-berliner-u-bahnhoefen-nach-u-bahnlinien-2017.xlsx" 
+
+
+##  The original files have been saved in the github repository
+## for the project: https://github.com/Olesiewitch/SPL-Project
+
+## The data has been merge into one file according to the names of the districts.The names 
+## of the colums have been keep according to the original file but translated into English. 
+
+## Processing of missing data 
+
+
