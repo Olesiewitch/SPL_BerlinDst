8f4420c24adfb5768c2b7f922c33f85b5a71a8f9
qier <qier@qiers-MacBook.local>
2019-01-09 09:23:40 +0100

    updating

--- Getting and Cleaning and Data/Ladestationen-data cleaning.R
+++ Getting and Cleaning and Data/Ladestationen-data cleaning.R
@@ -1,4 +1,3 @@
-setwd("/Users/qier/Downloads")
 library(readxl)
 ladestationen = read_excel("ladestationen.xls")
 Postleitzahlen = read_excel("ZuordnungderBezirkezuPostleitzahlen.xls")
@@ -12,6 +11,9 @@ Bezirk[7]="Tempelhof-Schöneberg";Bezirk<- Bezirk[-8];Bezirk
 
 class(Bezirk)#charachter
 
+#extract post code in column 'ladestationen$Adresse', where information is listed as 
+#e.g. "Malteserstrasse 136<U+2013>138, 12249 Berlin", and create a new column with post code
+
 adress=strsplit(ladestationen$Adresse,"[,]");adress
 adress1=c()
 for(i in 1:length(adress)) {
@@ -23,41 +25,50 @@ adress11<-c()
 for(i in 1:length(adress)) {
   adress11[i]<- adress1[[i]][2] 
 }   ;  adress11 
-
+#create a new dataframe with the name ladestationen1 with the new column of post code
 ladestationen1=data.frame(ladestationen,adress11)
 View(ladestationen1)
 
-a1<-as.numeric(c(Postleitzahlen$X__2[6:8],Postleitzahlen$X__3[6:8],
-     Postleitzahlen$X__4[6:8],Postleitzahlen$X__5[6:8],
-     Postleitzahlen$X__6[6:8],Postleitzahlen$X__7[6:8],
-     Postleitzahlen$X__8[6:7],Postleitzahlen$X__9[6:7],
-     Postleitzahlen$X__10[6:7],Postleitzahlen$X__11[6:7]))
-a2<-as.numeric(c(Postleitzahlen$X__2[10:11],Postleitzahlen$X__3[10:11],
-                Postleitzahlen$X__4[10:11],Postleitzahlen$X__5[10:11],
-                Postleitzahlen$X__6[10:11],Postleitzahlen$X__7[10],
-                Postleitzahlen$X__8[10],Postleitzahlen$X__9[10],
-                Postleitzahlen$X__10[10],Postleitzahlen$X__11[10]));b
+#extract the post code of each district from the original excel file 'ZuordnungderBezirkezuPostleitzahlen'
 
-a3<-as.numeric(unlist(as.list(Postleitzahlen[13:15,3:11])))
-a4<-as.numeric(unlist(as.list(Postleitzahlen[17:20,3:11])))
-a5<-as.numeric(unlist(as.list(Postleitzahlen[22:23,3:11])))
-a6<-as.numeric(unlist(as.list(Postleitzahlen[25:27,3:11])))
-a7<-as.numeric(unlist(as.list(Postleitzahlen[29:32,3:11])))
-a8<-as.numeric(unlist(as.list(Postleitzahlen[34:36,3:11])))
-a9<-as.numeric(unlist(as.list(Postleitzahlen[38:39,3:11])))
-a10<-as.numeric(unlist(as.list(Postleitzahlen[41:42,3:11])))
-a11<-as.numeric(unlist(as.list(Postleitzahlen[44:45,3:11])))
-a12<-as.numeric(unlist(as.list(Postleitzahlen[47:48,3:11])))
+district01<-as.numeric(unlist(as.list(Postleitzahlen[6:8,3:12])))
+district02<-as.numeric(unlist(as.list(Postleitzahlen[10:11,3:12])))
+district03<-as.numeric(unlist(as.list(Postleitzahlen[13:15,3:12])))
+district04<-as.numeric(unlist(as.list(Postleitzahlen[17:20,3:12])))
+district05<-as.numeric(unlist(as.list(Postleitzahlen[22:23,3:12])))
+district06<-as.numeric(unlist(as.list(Postleitzahlen[25:27,3:12])))
+district07<-as.numeric(unlist(as.list(Postleitzahlen[29:32,3:12])))
+district08<-as.numeric(unlist(as.list(Postleitzahlen[34:36,3:12])))
+district09<-as.numeric(unlist(as.list(Postleitzahlen[38:39,3:12])))
+district10<-as.numeric(unlist(as.list(Postleitzahlen[41:42,3:12])))
+district11<-as.numeric(unlist(as.list(Postleitzahlen[44:45,3:12])))
+district12<-as.numeric(unlist(as.list(Postleitzahlen[47:48,3:12])))
 
-Postleitzhal<- as.data.frame(cbind(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12))
+Postleitzhal<- as.data.frame(cbind(district01,district02,district03,district04,
+                                   district05,district06,district07,district08,
+                                   district09,district10,district11,district12))
 
-count<-rep(0,12)
-
+#compare the post code of each ladestation, and assgin a district to each ladestation
+dist_lade= rep(0,length(ladestationen1$adress11))
 for(i in 1:length(ladestationen1$adress11)){
-  if(ladestationen1$adress11[i] %in% Postleitzhal[,i]){
-    count[i]<-count[i]+1
+  for(j in 1:12){
+    if(ladestationen1$adress11[i] %in% Postleitzhal[,j]){
+      dist_lade[i]=j
     }
-};count 
+  }
+}
+sum(dist_lade!=0)
+
+#count the number of ladestation in each district
+count=rep(0,12)
+for(i in 1:12){
+  for(j in 1:length(dist_lade)){
+    if(dist_lade[j]==i)
+      count[i]=count[i]+1
+  }
+};count
+
+
 
 
 
