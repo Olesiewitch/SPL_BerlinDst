f83c3892a0bca6a9b055b08fbb0b595c1a9f9ca4
Olesiewitch <malgorzata.olesiewicz@gmail.com>
2019-02-28 10:07:35 +0100

    cleaning

--- Dirstrict Data/combining steps.R
+++
@@ -1,4 +0,0 @@
-a=as.numeric(steps1_4$c.zips1..zips2..zips3..zips4.)
-b=as.numeric(as.character(steps5_7$c.zips5..zips6..zips7.))
-c=as.numeric(steps8_11$c.zips8..zips9..zips10..zips11.)
-zips=c(a,b,c)
